6a4da797729b369b7323f5aa5d304172be61ec0e
maniolias <cedric.rumiano@gmail.com>
2016-05-23 17:21:05 +0200

    Template + splash screen and menu

--- src/processing/UserControlerBean.java
+++ src/processing/UserControlerBean.java
@@ -14,24 +14,22 @@ import dao.fabric.DaoFabric;
 import dao.instance.UserDao;
 
 @ManagedBean
-@ApplicationScoped // Utilisation de application scope afin d'offrir un point d'entrée unique à l'ensemble des clients
+@ApplicationScoped
 public class UserControlerBean {
 	private UserDao userDao;
+	
 	public UserControlerBean() {
 		this.userDao=DaoFabric.getInstance().createUserDao();
 	}
+	
 	public String checkUser(LoginBean loginBean){
 		UserModelBean user = this.userDao.checkUser(loginBean.getLogin(), loginBean.getPwd());
 		if( user!=null){
-			//récupère l'espace de mémoire de JSF
 			ExternalContext externalContext = FacesContext.getCurrentInstance().getExternalContext();
 			Map<String, Object> sessionMap = externalContext.getSessionMap();
-			//place l'utilisateur dans l'espace de mémoire de JSF
 			sessionMap.put("loggedUser", user);
-			//redirect the current page
 			return "userdisplay.xhtml";
 		}else{
-			//redirect the current page
 			return "userLogin.xhtml";
 		}
 	}
