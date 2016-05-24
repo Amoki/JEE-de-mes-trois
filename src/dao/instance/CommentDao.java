024df59a4ca591006ad5b6b4535d8f59eec3fa42
Liarick <j.lithyum@gmail.com>
2016-05-24 11:19:54 +0200

    ajout comment

---
+++ src/dao/instance/CommentDao.java
@@ -0,0 +1,25 @@
+package dao.instance;
+
+import java.sql.Connection;
+
+public class CommentDao {
+	private Connection connection;
+	private String dB_HOST;
+	private String dB_PORT;
+	private String dB_NAME;
+	private String dB_USER;
+	private String dB_PWD;
+
+	public CommentDao(String DB_HOST,String DB_PORT, String DB_NAME,String DB_USER,String DB_PWD) {
+		dB_HOST = DB_HOST;
+		dB_PORT = DB_PORT;
+		dB_NAME = DB_NAME;
+		dB_USER = DB_USER;
+		dB_PWD = DB_PWD;
+	}
+	
+	public void addComment() {
+	// TODO
+		
+	}
+}
